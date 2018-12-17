86ca1bc6ce1ab3d72a73d0b9b7e32a9e56a2d7bd
HackerRank <hackerrank@example.com>
2018-12-17 19:39:28 +0000

    Saved at Mon Dec 17 2018 14:39:27 GMT-0500 (Eastern Standard Time)

--- src/measurements/measurement-store.js
+++ src/measurements/measurement-store.js
@@ -35,7 +35,7 @@ export function queryDateRange(from, to) {
   let startIndex = -1;
   for (let i = 0; i < data.length; i++) {
       let measurement = data[i];
-      measur
+      let d
   }
   throw new HttpError(501);
 }
