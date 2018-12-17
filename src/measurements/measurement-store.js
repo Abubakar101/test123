63f7bf877cd12b5a4797cff0b77763677cb1a568
HackerRank <hackerrank@example.com>
2018-12-17 19:37:25 +0000

    Saved at Mon Dec 17 2018 14:37:24 GMT-0500 (Eastern Standard Time)

--- src/measurements/measurement-store.js
+++ src/measurements/measurement-store.js
@@ -32,6 +32,6 @@ export function fetch(timestamp) {
  * @param {Date} end Upper bound for the query, exclusive
  */
 export function queryDateRange(from, to) {
-  
+  l
   throw new HttpError(501);
 }
