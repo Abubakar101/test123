072737afc0bb2d9843223b3e4f375faff3d427ba
HackerRank <hackerrank@example.com>
2018-12-17 19:43:32 +0000

    Saved at Mon Dec 17 2018 14:43:32 GMT-0500 (Eastern Standard Time)

--- src/measurements/measurement-store.js
+++ src/measurements/measurement-store.js
@@ -32,7 +32,7 @@ export function fetch(timestamp) {
  * @param {Date} end Upper bound for the query, exclusive
  */
 export function queryDateRange(from, to) {
-  let startIndex = -1;
+  let  = -1;
   for (let i = 0; i < data.length; i++) {
       let measurement = data[i];
       let date = measurement.getMetric('timestampe');
