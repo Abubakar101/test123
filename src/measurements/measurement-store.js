83ff194b312ded78f2b79982febb72514c362d53
HackerRank <hackerrank@example.com>
2018-12-17 19:33:30 +0000

    Saved at Mon Dec 17 2018 14:33:30 GMT-0500 (Eastern Standard Time)

--- src/measurements/measurement-store.js
+++ src/measurements/measurement-store.js
@@ -18,7 +18,7 @@ export function add(measurement) {
  * @returns {Measurement} measurement for the particular date
  */
 export function fetch(timestamp) {
-  let measurement = data.find(measurement => measurement.getMetric('timestamp')getMetric === timestamp);
+  let measurement = data.find(measurement => measurement.getMetric('timestamp') === timestamp);
   throw new HttpError(501);
 }
 
