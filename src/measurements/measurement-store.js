b315ce02ea003d08f223af6442f8582eed9f7269
HackerRank <hackerrank@example.com>
2018-12-17 19:54:31 +0000

    Saved at Mon Dec 17 2018 14:54:31 GMT-0500 (Eastern Standard Time)

--- src/measurements/measurement-store.js
+++ src/measurements/measurement-store.js
@@ -19,7 +19,9 @@ export function add(measurement) {
 export function fetch(timestamp) {
     
   let measurement = data.find(measurement => {
+      console.log('+++++++++++++++++++++++++++++++++++++')
       
+      console.log('+++++++++++++++++++++++++++++++++++++')
       measurement.getMetric('timestamp').getTime() === timestamp.getTime()
   });
   if (measurement) {
